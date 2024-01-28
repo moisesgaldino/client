755316898ca76c0fb8bc2387def305e25bb52566
Moises Galdino <90233062+moisesgaldino@users.noreply.github.com>
2024-01-28 11:49:52 -0300

    launching the login and registration pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import "antd/dist/antd";
+import { Button } from "antd";
 function App() {
   return (
-    <div className="App">
-        <h1>SYS MEDICAL</h1>
+    <div className="App p-5">
+      <h1>SYS MEDICAL</h1>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
